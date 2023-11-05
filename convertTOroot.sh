0aa48a62b5e1baf832e6e17ac98de96c306eefd8
ltsai323 <johnson20050@gmail.com>
2023-11-05 14:31:51 +0800

    prevent executing gunzip if .gz file not found

--- convertTOroot.sh
+++ convertTOroot.sh
@@ -2,19 +2,20 @@
 loadDIR=$1
 loaddir=`realpath $loadDIR`
 workdir=$loaddir/Events
-exec_file=/afs/cern.ch/work/l/ltsai/Work/HiggsCombine/CMSSW_11_3_4/src/MG5_aMC_v3_4_2/ExRootAnalysis/ExRootLHEFConverter 
+exec_file=/wk_cms3/ltsai/wk_cms/ltsai/CMSSW/Run3Winter22/CMSSW_12_2_4/src/MG5_aMC_v3_5_0/ExRootAnalysis/ExRootLHEFConverter
 
 function the_exit() { echo [ERROR] $1; exit; }
 
 
 cd $workdir
-# -tr is important to keep the creation order in json creation
-for trgdir in `ls -tr -d $workdir/*/`; do
+# -t is important to keep the creation order in json creation
+for trgdir in `ls -t -d $workdir/*/`; do
     out_root=${trgdir%/}.root
     echo processing $out_root
     loadFILE=${trgdir}/events.lhe.gz
     processFILE=${trgdir}/events.lhe
-    gzip -d $loadFILE # ignore the result
+    if [ -z $loadFILE ]; then gunzip -d $loadFILE ; fi
+    if [ ! -z $processFILE ]; then continue; fi
     
 
     $exec_file $processFILE $out_root > /dev/null || \
