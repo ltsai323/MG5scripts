cf41b0ba8e1f0d21e63d919cff3d9a3ef5295625
ltsai323 <johnson20050@gmail.com>
2023-11-08 15:05:22 +0800

    optimize executing message

--- convertTOroot.sh
+++ convertTOroot.sh
@@ -8,16 +8,22 @@ function the_exit() { echo [ERROR] $1; exit; }
 
 
 cd $workdir
+echo change to $workdir
 # -t is important to keep the creation order in json creation
 for trgdir in `ls -t -d $workdir/*/`; do
     out_root=${trgdir%/}.root
-    echo processing $out_root
     loadFILE=${trgdir}/events.lhe.gz
     processFILE=${trgdir}/events.lhe
-    if [ -z $loadFILE ]; then gunzip -d $loadFILE ; fi
-    if [ ! -z $processFILE ]; then continue; fi
-    
 
+    echo -e "\n\n------ checking directory `basename $trgdir`"
+    if [ -e $loadFILE ]; then gzip -d $loadFILE ;
+    else echo "[File not found - ignore unzip] `basename $loadFILE`"   ; fi
+    if [ ! -e $processFILE ]; then
+        echo "[File not found - skip convert] `basename $processFILE`" ; continue; fi
+    if [ -e $out_root ]; then
+        echo "[File existed - skip convert] `basename $out_root`"      ; continue; fi
+
+    echo "[status] `basename $out_root`"
     $exec_file $processFILE $out_root > /dev/null || \
         the_exit "[INFO] $out_root failed creation"
 done
